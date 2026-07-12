130c9d00c47918924bd2f710cd329e926ff8646e
kyoo-147 <starbyteservices010@gmail.com>
2026-07-12 13:14:18 +0700

    Fix Task 1 review findings

---
+++ target/debug/deps/together.d
@@ -0,0 +1,5 @@
+D:\working\Together\Product\target\debug\deps\together.d: cli\src\main.rs
+
+D:\working\Together\Product\target\debug\deps\together.exe: cli\src\main.rs
+
+cli\src\main.rs:
---
+++ target/debug/together.d
@@ -0,0 +1,1 @@
+D:\working\Together\Product\target\debug\together.exe: D:\working\Together\Product\cli\src\main.rs
